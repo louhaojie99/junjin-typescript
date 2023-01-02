9cb4e4dfca7a322f98e62b3fb88975b47881f0c7
lhj <851681631@qq.com>
2023-01-02 16:10:36 +0800

    feat: typescript 原始类型标注学习

---
+++ （一）类型基础/1_原始类型.ts
@@ -0,0 +1,21 @@
+/** 字符型 */
+const str: string = "hello typescript";
+
+/** 数值型 */
+const num: number = 1000;
+
+/** 布尔型 */
+const flag: boolean = false;
+
+/** null（表示的是一个有意义的具体类型值） */
+const nu: null = null;
+
+/** undefined（表示的是一个有意义的具体类型值） */
+const un: undefined = undefined;
+
+/** Symbol */
+const sybolValue: symbol = Symbol("louhaojie");
+
+/** Bigint */
+const bigintValue1: bigint = 9007199254740991n; // 科学计数法
+const bigintValue2: bigint = BigInt(9007199254740991);
